5dc0af30d9f6ab40dbd90c8d2f53d935db58ca0b
felix <sparkyxu@hotmail.com>
2018-11-28 21:50:05 -0800

    update footer

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 $('.main-carousel').flickity({
-    // options
-    cellAlign: 'left',
-    contain: true,
-    wrapAround: true
-});
+  cellAlign: 'left',
+  contain: true,
+  wrapAround: true
+})
 
+$('#submit').click(function () {
+  alert('thank you for your input.')
+})
 
-$("#submit").click(function () {
-    alert("thak you for your input.");
-});
+$('#emailbox').submit(function (event) {
+  event.preventDefault()
+
+  if ($('#enteremail').val() == '') {
+    alert('Please enter a valid email address!')
+  } else {
+    alert('Thanks for your subscription!')
+  }
+})
